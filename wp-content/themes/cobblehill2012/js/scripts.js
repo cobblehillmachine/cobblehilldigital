c321a0deb01fb1f4ed9c4dad8e4087a43590ea38
samanthavandapuye <samantha.vandapuye@gmail.com>
2013-06-17 15:51:42 -0400

    work js chrome fix2

--- wp-content/themes/cobblehill2012/js/scripts.js
+++ wp-content/themes/cobblehill2012/js/scripts.js
@@ -1,35 +1,6 @@
 $(document).ready(function() {
 	$('body').fadeIn('slow');
-		//Calculate the height of <header>
-		//Use outerHeight() instead of height() if have padding
-		var aboveHeight = $('.header-image-wrap').height();
-		var slidesHeight = $('.work-slides').height();
-		var leftcontentHeight = $('.single-content').height();
-		var newH = slidesHeight - 475 +'px';
-		$('.single-content').css('height', leftcontentHeight) ;
-	//when scroll
-		$(window).scroll(function(){
 
-			//if scrolled down more than the header’s height
-		if ($(window).scrollTop() > aboveHeight){
-
-				$('.single-content').addClass('fixed');
-				if ($(window).scrollTop() > slidesHeight){
-
-					$('.single-content').removeClass('fixed');
-					$('.single-content').css({'margin-top':newH});
-
-					 } else {
-
-					$('.single-content').addClass('fixed');
-					$('.single-content').css({'margin-top':0});
-				}
-		} else {
-				$('.single-content').removeClass('fixed');
-		}
-
-
-		});
 	$('img[title]').each(function() { $(this).removeAttr('title'); });
 	$('.team-member .img-cont').on({
 		mouseenter: function(){$(this).children('.overlay').show();},
@@ -88,5 +59,36 @@ $(document).ready(function() {
 	$(document).ready(function(){
 		$("label").inFieldLabels();
 		$(".work-slides").fitVids();
+	});
+});
+
+$(document).ready(function() {
+	var aboveHeight = $('.header-image-wrap').height();
+	var slidesHeight = $('.work-slides').height();
+
+	var newH = slidesHeight - 475 +'px';
+
+
+	$(window).scroll(function(){
+
+
+	if ($(window).scrollTop() > aboveHeight){
+
+			$('.single-content').addClass('fixed');
+			if ($(window).scrollTop() > slidesHeight){
+
+				$('.single-content').removeClass('fixed');
+				$('.single-content').css({'margin-top':newH});
+
+				 } else {
+
+				$('.single-content').addClass('fixed');
+				$('.single-content').css({'margin-top':0});
+			}
+	} else {
+			$('.single-content').removeClass('fixed');
+	}
+
+
 	});
 });
